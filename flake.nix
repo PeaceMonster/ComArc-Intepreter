2a2ae9c5203ace4b0faeb60d34059c2952b8d49d
Mikkel Ugilt <ugilt3@gmail.com>
2025-02-25 16:25:34 +0100

    Flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+             pkgs.lld
+             pkgs.pkg-config
+            ];
+          };
+        }
+      );
+
+}
